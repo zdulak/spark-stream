2bdd9efd61576591daf28646aa364eb2c914bfc7
zdulak <dpzdulski@gmail.com>
2022-04-12 09:54:10 +0200

    Add first spark streaming project

---
+++ src/main/scala/FileStream.scala
@@ -0,0 +1,24 @@
+object FileStream  extends App {
+  import org.apache.spark.sql.SparkSession
+  val spark = SparkSession
+    .builder()
+    .appName("Wed")
+    .master("local[*]")
+    .getOrCreate()
+
+  val path = if (args.length > 0) args(0) else "data"
+  
+  import org.apache.spark.sql.types._
+  val schema = StructType(
+    StructField("id", LongType, nullable = false) ::
+    StructField("name", StringType, nullable = false) :: Nil
+  )
+
+  val stream = spark.readStream
+    .format("csv")
+    .option("header", "true")
+    .schema(schema)
+    .load(path)
+
+  stream.writeStream.format("console").start.awaitTermination()
+}
